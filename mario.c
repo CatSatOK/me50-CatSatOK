e77f961015e80127694ba4d035d8d323d05825ad
CatSatOK <CatSatOK@users.noreply.github.com>
2020-09-18 14:53:27 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -4,22 +4,24 @@
 int main(void)
 {
     int number, line, hash, dot;
-   do
-   {
-   number = get_int("What height do you want the pyramid?\n");
-   }
-   while (number < 1 || number > 8);
-
-   for(line = 0; line < number; line++)
-   {
-       for(dot = 0; dot < number - line - 1; dot++)
+    //request input
+    do
+    {
+        number = get_int("What height do you want the pyramid?\n");
+    }
+    //ensure keeps to size requirements
+    while (number < 1 || number > 8);
+    //make pyramid
+    for (line = 0; line < number; line++)
+    {
+        for (dot = 0; dot < number - line - 1; dot++)
         {
             printf(" ");
         }
-       for(hash = 0; hash <= line; hash++)
-       {
-           printf("#");
-       }
-       printf("\n");
-   }
+        for (hash = 0; hash <= line; hash++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
