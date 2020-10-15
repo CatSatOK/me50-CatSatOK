2ae8ab130cacd6bc5a3e2d8fb190d9c67056ad32
CatSatOK <CatSatOK@users.noreply.github.com>
2020-10-15 10:52:02 +0000

    automated commit by submit50

--- runoff.c
+++ runoff.c
@@ -132,8 +132,8 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-           preferences[voter][rank] = i;
-           return true;
+            preferences[voter][rank] = i;
+            return true;
         }
     }
     return false;
