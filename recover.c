9afc9d86e6d47ca9d2574317790b86d5e3f5c165
CatSatOK <CatSatOK@users.noreply.github.com>
2020-10-28 12:27:20 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -58,6 +58,10 @@ int main(int argc, char *argv[])
     {
         if(isJpgHeader(buffer))
         {
+            if(outputPtr != NULL)
+            {
+                fclose(outputPtr);
+            }
             sprintf(filename, " %03i.jpg", jpgCounter);
             outputPtr = fopen(filename, "w");
             jpgCounter++;
