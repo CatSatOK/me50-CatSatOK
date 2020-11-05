7d37317479720981f9766d9376539d5756f9e633
CatSatOK <CatSatOK@users.noreply.github.com>
2020-11-05 09:37:41 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -5,80 +5,74 @@
 
 #define BLOCKSIZE 512
 
-//make input error return
-int inputError()
-{
-    printf("Usage: ./recover image\n");
-    return 1;
-}
-
-//make file error return
-int fileError(char filename[])
-{
-    printf ("Unable to open file: %s\n", filename);
-    return 1;
-}
-
-//make if jpg header
-bool isJpgHeader (uint8_t buffer[])
-{
-    return buffer[0] == 0xff
-    && buffer[1] == 0xd8
-    && buffer[2] == 0xff
-    && (buffer[3] & 0xf0) ==0xe0;
-}
-
 //main
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        return inputError();
+        printf("Usage: ./recover image\n");
+        return 1;
     }
 
-    char * inputFile = argv[1];
-    if (inputFile == NULL)
+    FILE *file = fopen(argv[1], "r");
+    if(file == NULL)
     {
-        return inputError();
+        printf("File opening error\n");
+        return 1;
     }
 
-    //make file pointer & error check
-    FILE* inputPtr = fopen(inputFile, "r");
-    if (inputPtr == NULL)
-    {
-        return fileError(inputFile);
-    }
+    typedef uint8_t BYTE;
+    BYTE buffer[BLOCKSIZE];
 
-    char filename[8];
-    FILE* outputPtr = NULL;
-    uint8_t buffer[BLOCKSIZE];
-    int jpgCounter = 0;
+    //have valid file so start checking blocks
+    size_t bytes_read;
 
-    while (fread (buffer, sizeof(uint8_t), BLOCKSIZE, inputPtr) || feof(inputPtr) == 0)
+    bool is_first_jpeg = false;
+    FILE *current_file;
+    char current_filename[100];
+    int current_filenumber = 0;
+    bool found_jpeg = false;
+
+    //open memory card
+    while (true)
     {
-        if(isJpgHeader(buffer))
+         //repeat until end of card
+        //read 512 bytes in
+        bytes_read = fread(buffer, sizeof(BYTE), BLOCKSIZE, file);
+        if (bytes_read == 0)
         {
-            if(outputPtr != NULL)
+            break; //end of file
+        }
+
+        //if start of new jpeg
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0x44 && (buffer[3] & 0xf0) == 0xe0)
+        {
+            found_jpeg = true;
+            //if first jpeg mark first jpeg
+            if (!is_first_jpeg)
             {
-                fclose(outputPtr);
+                is_first_jpeg = true;
             }
-            sprintf(filename, " %03i.jpg", jpgCounter);
-            outputPtr = fopen(filename, "w");
-            jpgCounter++;
+            //else close current file, open new file
+            else
+            {
+                fclose(current_file);
+            }
+            sprintf(current_filename, "%03i.jpg", current_filenumber);
+            current_file = fopen(current_filename, "w");
+            fwrite(buffer, sizeof(BYTE), bytes_read, current_file);
+            current_filenumber++;
         }
-        if(outputPtr != NULL)
+        else
         {
-            fwrite(buffer, sizeof(buffer), 1, outputPtr);
+           //else if already found jpeg keep writing to it
+           if (found_jpeg)
+           {
+              fwrite(buffer, sizeof(BYTE), bytes_read, current_file);
+           }
         }
     }
-     if(inputPtr != NULL)
-    {
-        fclose(inputPtr);
-    }
-
-    if(outputPtr != NULL)
-    {
-        fclose(outputPtr);
-    }
+    fclose(file);
+    fclose(current_file);
     return 0;
 }
