5b102340c6511f4f60026e0105b8cb7709d52327
CatSatOK <CatSatOK@users.noreply.github.com>
2020-11-26 14:16:01 +0000

    automated commit by submit50

---
+++ 2.sql
@@ -0,0 +1,5 @@
+--Emma Stone birth year
+
+SELECT birth
+FROM people
+WHERE name = 'Emma Stone';
---
+++ 3.sql
@@ -0,0 +1,6 @@
+--titles released on or after 2018 alphabetical order
+
+SELECT title
+FROM movies
+WHERE year >= 2018
+ORDER BY title;
